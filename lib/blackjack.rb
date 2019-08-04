ccf9fef89221008ebee138de2cdfbf0363aa2bc3
Joel Mendoza <joelmendza173@gmail.com>
2019-08-04 22:02:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,19 +15,24 @@ end
 
 def prompt_user
   # code #prompt_user here
-  puts "Type 'h' to hit or 's' to stay".
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game(final_total)
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
 
 def hit?
