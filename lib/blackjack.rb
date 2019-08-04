650b2690b753aa52d52b6249b82e31b15109191e
Joel Mendoza <joelmendza173@gmail.com>
2019-08-04 21:58:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,11 +30,9 @@ end
 
 def initial_round
   # code #initial_round here
-  card1 = deal_card
-  card2 = deal_card
-  total = card1 + card2
+  total = deal_card + deal_card
   display_card_total(total)
-  return total
+  total
 end
 
 def hit?
