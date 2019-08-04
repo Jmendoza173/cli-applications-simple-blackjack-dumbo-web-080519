48ac136e353b005b4e4b40852055e20d6e58ff75
Joel Mendoza <joelmendza173@gmail.com>
2019-08-04 21:59:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,14 +23,12 @@ def get_user_input
   gets.chomp
 end
 
-def end_game(card_total)
+def end_game(final_total)
   # code #end_game here
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
-  
 end
 
 def hit?
